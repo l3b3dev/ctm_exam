b8648023d4d2ae0febd11a08ed267700a6ed6b0e
lebedev <yuriy.lebedev@drips.com>
2021-05-13 17:21:37 -0400

    apis working

---
+++ ctmexam/Controllers/FizzBuzzController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ctmexam.Services;
+using ctmexam.Services.Interfaces;
+
+namespace ctmexam.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FizzBuzzController : ControllerBase
+    {
+        //TODO: inject this later
+        private readonly IFizzBuzzService _service = new FizzBuzzService();
+
+        public IEnumerable<string> Get(int n)
+        {
+            return _service.FizzBuzz(n);
+        }
+    }
+}
